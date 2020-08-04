a063cc78f482705febcb6546341460a6742200be
manasvi762 <64463973+manasvi762@users.noreply.github.com>
2020-08-04 18:36:52 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -48,3 +48,15 @@ animMeter();
             }
         });
     });
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
